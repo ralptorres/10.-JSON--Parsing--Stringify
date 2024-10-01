767da95345ecd96752c3de59d1e6afc38f56d5b2
ralptorres <ralp.torres@gmail.com>
2024-10-01 21:41:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let person = [
+  {
+    fName: "Ralp",
+    lName: "Torres",
+    age: 30,
+  },
+  {
+    fName: "Roche",
+    lName: "Torres",
+    age: 30,
+  },
+  {
+    fName: "Raiven",
+    lName: "Villaflores",
+    age: 19,
+  },
+];
+
+for (i = 0; i < person.length; i++) {
+  console.log(`First Name : ${person[i].fName}`);
+  console.log(`Last Name  : ${person[i].lName}`);
+  console.log(`Age        : ${person[i].age}`);
+  console.log(` `);
+}
